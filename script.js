3fb5b6d5ef0596bccd80c88be54cb788ecd2e9bf
Premforreal <93695810+Premforreal@users.noreply.github.com>
2022-06-27 15:05:31 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,10 +1,14 @@
+/*
+A JavaScript class is not an object.
+It is a template for JavaScript objects.
+*/
 class Calculator {
-  constructor(previousOperandTextElement, currentOperandTextElement) {
-    this.previousOperandTextElement = previousOperandTextElement
-    this.currentOperandTextElement = currentOperandTextElement
-    this.clear()
-  }
-
+    constructor(previousOperandTextElement, currentOperandTextElement) {
+      this.previousOperandTextElement = previousOperandTextElement
+      this.currentOperandTextElement = currentOperandTextElement
+      this.clear()
+    }
+  //when we hit clear button these values will be defaulted.
   clear() {
     this.currentOperand = ''
     this.previousOperand = ''
@@ -14,16 +18,17 @@ class Calculator {
   delete() {
     this.currentOperand = this.currentOperand.toString().slice(0, -1)
   }
-
+    //selects number when we click a button
   appendNumber(number) {
-    if (number === '.' && this.currentOperand.includes('.')) return
-    this.currentOperand = this.currentOperand.toString() + number.toString()
+    if (number === '.' && this.currentOperand.includes('.')) return //this will stop period from appending multiple times
+    this.currentOperand = this.currentOperand.toString() + number.toString() //because JS will try to add numbers
   }
-
+    //selects the operation when we click
   chooseOperation(operation) {
-    if (this.currentOperand === '') return
+    if (this.currentOperand === '') return //stops from entering mutliple operands
+    //if previous operand is not empty string do the calculation and append operand
     if (this.previousOperand !== '') {
-      this.compute()
+      this.compute()  
     }
     this.operation = operation
     this.previousOperand = this.currentOperand
@@ -56,16 +61,19 @@ class Calculator {
     this.previousOperand = ''
   }
 
+
+  //to add commas after if a number becomes "longer" 
   getDisplayNumber(number) {
-    const stringNumber = number.toString()
-    const integerDigits = parseFloat(stringNumber.split('.')[0])
-    const decimalDigits = stringNumber.split('.')[1]
+    const stringNumber = number.toString() // because decimal numbers like 0.2 are not appending
+    const integerDigits = parseFloat(stringNumber.split('.')[0])//the number (now string) will be split and added to array
+    const decimalDigits = stringNumber.split('.')[1] //appending number after decimal place
     let integerDisplay
-    if (isNaN(integerDigits)) {
+    if (isNaN(integerDigits)) { 
       integerDisplay = ''
     } else {
-      integerDisplay = integerDigits.toLocaleString('en', { maximumFractionDigits: 0 })
+      integerDisplay = integerDigits.toLocaleString('en', { maximumFractionDigits: 0 /*there can be 0 decimal places after a decimal*/})
     }
+        //check if we have decimal digits
     if (decimalDigits != null) {
       return `${integerDisplay}.${decimalDigits}`
     } else {
@@ -73,14 +81,15 @@ class Calculator {
     }
   }
 
+  //updatesDisplay elements when called
   updateDisplay() {
     this.currentOperandTextElement.innerText =
       this.getDisplayNumber(this.currentOperand)
     if (this.operation != null) {
       this.previousOperandTextElement.innerText =
-        `${this.getDisplayNumber(this.previousOperand)} ${this.operation}`
+        `${this.getDisplayNumber(this.previousOperand)} ${this.operation}` //$ is document.getelementbyid
     } else {
-      this.previousOperandTextElement.innerText = ''
+      this.previousOperandTextElement.innerText = '' //to clear previous operand after calculation
     }
   }
 }
@@ -96,12 +105,15 @@ const currentOperandTextElement = document.querySelector('[data-current-operand]
 
 const calculator = new Calculator(previousOperandTextElement, currentOperandTextElement)
 
+//adding event listener for buttons and call updateDisplay()
 numberButtons.forEach(button => {
   button.addEventListener('click', () => {
     calculator.appendNumber(button.innerText)
     calculator.updateDisplay()
   })
 })
+
+
 
 operationButtons.forEach(button => {
   button.addEventListener('click', () => {
